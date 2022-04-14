2d9de2ede9002735217c61887ba7270d787c55aa
Yusuke Yagyu (gyugyu) <gyugyu@pepabo.com>
2022-04-14 14:18:12 +0900

    initial commit

---
+++ lib/mimitabu.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mimitabu/version"
+
+module Mimitabu
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mimitabu/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Mimitabu
+  VERSION = "0.1.0"
+end
